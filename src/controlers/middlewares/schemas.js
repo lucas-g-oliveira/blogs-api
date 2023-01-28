82cbb9afa09aaef69f0a8177882eace2124eaa30
Lucas G <lucasg.devmail93@gmail.com>
2023-01-28 19:54:50 -0300

    env: create schema validations

---
+++ src/controlers/middlewares/schemas.js
@@ -0,0 +1,37 @@
+const Joi = require('joi');
+
+const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+
+// POST login
+const login = Joi.object().keys({
+  email: Joi.string().regex(emailRegex).required(),
+  password: Joi.string().min(6).required(),
+});
+
+// GET simpleId
+const idValidate = Joi.number().required();
+
+// POST post
+const addPost = Joi.object().keys({
+  title: Joi.string().min(8).required(),
+  content: Joi.string().min(8).required(),
+  categoryId: Joi.array().items(Joi.number().integer().min(1)).length(2).required(),
+});
+
+// PUT post
+const setPost = Joi.object().keys({});
+
+// POST User
+const addUser = Joi.object().keys({
+  displayName: Joi.string().min(8).required(),
+  email: Joi.string().regex(emailRegex).required(),
+  password: Joi.string().min(6).required(),
+  image: null,
+});
+
+// POST category
+const addCategory = Joi.object().keys({
+  name: Joi.string().min(8).required(),
+});
+
+module.exports = { login, addUser, addPost, addCategory, idValidate, setPost };
